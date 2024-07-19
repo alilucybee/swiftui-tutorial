99ffcfec30b77df2d7eb9ba2ad943ef23be29c2d
Alison Barnett <alison_lucy_barnett@hotmail.com>
2024-07-19 15:17:31 +0100

    Display list of landmarks with detail view

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Alison Barnett on 18/07/2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,49 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Alison Barnett on 19/07/2024.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300.0)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.largeTitle)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: landmarks[0])
+}
